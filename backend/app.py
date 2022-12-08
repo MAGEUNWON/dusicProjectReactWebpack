80f8a72e66cdade75ec0db253e6d73239693daee
unknown <geunwon1947@gmail.com>
2022-12-08 15:43:56 +0900

    axios, cors 내용 정리

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,14 @@ app.config['JSON_AS_ASCII'] = False
 
 CORS(app,resources={r'*':{'origins':'http://localhost:3000'}},supports_credentials=True)
 # cors에러나서 설정 해준 것. frontend 서버인 localhost 3000번 에서만 데이터 띄우라고 해준 것
+#CORS는 서로 다른 출처의 리소스 접근 권한을 브라우저가 알 수 있도록 정리한 규칙. 모방 사이트를 제지하기 위해 필요한 제약 사항. 다른 출처란 domain, scheme, port를 포함한 것. 같은 도메인이라도 scheme 또는 port가 다르면 브라우저가 해당 요청을 다른 출처로 판단하고 CORS 정책을 기반으로 권한 여부를 확인. 
+#파이썬 flask 프레임워크에서 flask_cors 모듈을 이용해서 CORS를 적용 url 전체에 적용할 경우 CORS(app)만 해주면 전체 url에 대해서 CORS가 적용되어 다른 도메인에서 해당 url을 호출해서 사용하는데 문제가 없음.
+#CORS(app.resources={r'*':{'origins':'*'}}) 얘는 모든 곳에서 호출 허용
+#CORS(app.resources={r'*':{'origins':'http://localhost:3000'}}) 얘는 http://localhost:3000에서의 호출만 허용
+#CORS(app.resources={r'/_api/*':{'origins':'http://localhost:3000'}}) 얘는 /_api/의 하위 주소를 가지는 경우에만 호출이 가능. 즉 http://localhost:3000/_api/등의 주소를 호출할 때만 정상적으로 리소스 전달.
+#하나가 아닌 여러개의 도메인 주소를 허용할 때는 
+#CORS(app.resources={r'*':{'origins':['http://localhost:3000', http://localhost:5000']}})  배열로 여러개의 주소를 써주면 됨. 
+#이렇게 설정 한 후에도 CORS 에러가 계속 발생한다면 AJAX 호출 부분에서 CORS 설정이 잘못 되었을 경우가 많음. 그 쪽도 이슈를 확인. 
 
 @app.route('/<market>', methods=['GET'])
 def allSearch(market):
--- src/common/axiosSet.ts
+++ src/common/axiosSet.ts
@@ -1,7 +1,15 @@
 // 얘도 cors에러나서 설정 해준 것. backend 서버인 localhost 5000에 있는 api 자료를 axios로 가져오라고 설정 해준 것. 
 
 import axios from "axios";
+//React에서 OpenAPI를 이용한 통신을 할 때 Acios를 주로 사용
+//웹 브라우저와 Node.js를 위한 HTTP 비동기통신 라이브러리
+//백엔드와 프론트엔드 간 통신을 쉽게 하기 위해 사용되는 것으로 Ajax처럼 사용
+//Fetch와 Acios 둘 다 HTTP 요청을 처리하기 위한 자바스크립트 라이브러리
+//Fetch는 js에 내장되어 있기 때문에 별도의 import나 설치가 필요하지 않음. Acios는 설치 과정 필요 
+//Axios가 Fetch보다 브라우저 호환성이 좋음. Fetch에서 지원하지 않는 JSON 자동 변환, 응답시간 초과 설정 기능 등을 Axios에서 지원해줌. 
 
+
+//axiosSet이라는 인스턴스 생성. 전역적으로 사용할 수 있음. 
 const axiosSet = axios.create({
   baseURL: 'http://localhost:5000',
   headers: {
@@ -10,4 +18,6 @@ const axiosSet = axios.create({
   withCredentials: true,
 });
 
-export default axiosSet;
+export default axiosSet; //axios 인스턴스를 내보냄. 
+//중간에 다른 URL로 요청해야 하는 경우 axios.create를 통해 커스텀 객체를 만들어 사용할 수 있음. 
+//미리 url 설정된 상태로 만들어 놓고 이 인스터스를 가져와서 쓰면 됨. axiosSet 객체를 통한 axios 요청은 언제가 저 baseURL이 붙게되며 다른 요청과 구별할 수 있음. 
